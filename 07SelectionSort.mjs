436e4459ee07ee7d92a295c93004b533020eb703
MiguelHenriqueO <miguelhenriqueisaac@gmail.com>
2026-08-19 19:11:30 -0300

    update ED2

---
+++ 07SelectionSort.mjs
@@ -0,0 +1,35 @@
+let pass,comps,trocas
+
+function selectionSort(vetor){
+    pass = 0 , comps = 0, trocas = 0
+    for (let posSel = 0; posSel < vetor.length - 1; posSel++) {
+        pass++
+
+        let posMenor = posSel + 1
+
+        for(let i = posMenor + 1; i < vetor.length; i++){
+            
+            if(vetor[posMenor] > vetor[i]) posMenor = i
+            comps++
+
+
+        }
+
+        comps++
+        if(vetor[posSel] > vetor[posMenor]){
+            
+            [vetor[posSel], vetor[posMenor]] = [vetor[posMenor], vetor[posSel]]
+            trocas++
+        }
+        
+    }
+
+}
+
+let nums = [1,56,23,8898,3667,12,11,0,999]
+
+import {nomes} from "./Data/nomes-desord.mjs"
+
+selectionSort(nomes)
+console.log(nomes)
+console.log({pass, comps , trocas})
